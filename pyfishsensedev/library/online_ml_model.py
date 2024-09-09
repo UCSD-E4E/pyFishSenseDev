64107194426a529728f7203b881ef3c197a1c475
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-09-09 15:24:39 -0700

    feat: laser detector model download from hugging face

---
+++ pyfishsensedev/library/online_ml_model.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+from requests import get
+
+from pyfishsensedev.library.paths import CACHE_DIRECTORY
+
+
+class OnlineMLModel(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def _model_cache_path(self) -> Path:
+        return CACHE_DIRECTORY / "models"
+
+    @property
+    @abstractmethod
+    def _model_path(self) -> Path:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def _model_url(self) -> str:
+        raise NotImplementedError
+
+    def _download_file(self, url: str, path: Path) -> Path:
+        if not path.exists():
+            path.parent.mkdir(parents=True, exist_ok=True)
+
+            response = get(url)
+            with path.open("wb") as file:
+                file.write(response.content)
+
+        return path.absolute()
+
+    def download_model(self) -> Path:
+        return self._download_file(
+            self._model_url(),
+            self._model_path(),
+        ).as_posix()
---
+++ pyfishsensedev/library/paths.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from appdirs import user_cache_dir
+
+from pyfishsensedev import __version__
+
+
+def _get_cache_directory() -> Path:
+    directory = Path(
+        user_cache_dir(
+            appname="pyFishSenseDev",
+            appauthor="Engineers for Exploration",
+            version=__version__,
+        )
+    )
+
+    if not directory.exists():
+        directory.mkdir(exist_ok=True)
+
+    return directory
+
+
+CACHE_DIRECTORY = _get_cache_directory()
