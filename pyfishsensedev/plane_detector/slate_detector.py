4753726228afeb291089f5435770bda9e4e0e40f
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-09-14 12:20:19 -0700

    feat: slate plane detector stub

--- pyfishsensedev/plane_detector/slate_detector.py
+++ pyfishsensedev/plane_detector/slate_detector.py
@@ -1,6 +1,17 @@
+import numpy as np
+
+from pyfishsensedev.image.pdf import Pdf
 from pyfishsensedev.plane_detector.plane_detector import PlaneDetector
 
 
 class SlateDetector(PlaneDetector):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, image: np.ndarray[np.uint8], pdf: Pdf) -> None:
+        super().__init__(image)
+
+        self._pdf = pdf
+
+    def _get_points_image_space(self):
+        pass
+
+    def _get_points_body_space(self):
+        pass
