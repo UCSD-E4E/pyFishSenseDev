bf99891cb2fea98d768a6e1e9b61fb6f9c1dcc11
Ben McCalmon <bbmcca26@g.holycross.edu>
2024-09-14 16:42:38 -0700

    feat: homography for slate calibration

---
+++ pyfishsensedev/library/homography/image_matcher.py
@@ -0,0 +1,101 @@
+"""
+Inference use of SuperPoint+LightGlue
+Usage: Look at examples from example_inference.ipynb and process_data.py
+Due to licensing issues, make sure you are using the correct version of SuperPoint.
+"""
+
+import torch
+
+from pyfishsensedev.library.homography.models.lightglue import LightGlue
+from pyfishsensedev.library.homography.models.superpoint import (
+    SuperPoint as NonCommercialSuperPoint,
+)
+from pyfishsensedev.library.homography.models.superpoint_pytorch import SuperPoint
+from pyfishsensedev.library.homography.utils import Preprocessor, rbd
+
+
+class ImageMatcher:
+    def __init__(self, template: torch.Tensor, com_license=True, processing_conf={}):
+        self.image0 = template
+        self.feats0 = None
+        self.com_license = com_license
+        self.preprocess_conf = (
+            processing_conf["preprocess"] if "preprocess" in processing_conf else {}
+        )
+        self.matcher_conf = (
+            processing_conf["matcher"] if "matcher" in processing_conf else {}
+        )
+        self.extractor_conf = (
+            processing_conf["extractor"] if "extractor" in processing_conf else {}
+        )
+
+    def __call__(self, image1: torch.Tensor):
+        """Given a calibration image, return its matches with the template.
+        Input:
+            image1: torch.Tensor
+        Output:
+            feats0_matches
+            feats1_matches
+        """
+        # Preprocess our input image
+        preprocessor = Preprocessor(**self.preprocess_conf)
+        image1_preprocessed = preprocessor(image1)
+
+        with torch.no_grad():
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            torch.set_grad_enabled(False)
+
+            # SuperPoint+LightGlue
+            def_extractor_conf = {
+                "max_num_keypoints": None,
+            }
+            def_matcher_conf = {
+                "features": "superpoint",
+                "depth_confidence": -1,
+                "width_confidence": -1,
+                "filter_threshold": 0.5,  # 0.5
+            }
+            self.extractor_conf = {**def_extractor_conf, **self.extractor_conf}
+            self.matcher_conf = {**def_matcher_conf, **self.matcher_conf}
+            extractor = (
+                SuperPoint(**self.extractor_conf).eval().to(device)
+                if self.com_license
+                else NonCommercialSuperPoint(**self.extractor_conf).eval().to(device)
+            )
+            matcher = LightGlue(**self.matcher_conf).eval().to(device)
+
+            # Extract features
+            if not self.com_license:
+                print("    WARNING: USING THE NON-COMMERCIAL VERSION OF SUPERPOINT!")
+            if (
+                self.feats0 == None
+            ):  # so we don't have to extract template keypoints every time
+                self.feats0 = extractor.extract(self.image0.to(device))
+            else:
+                for k, _ in self.feats0.items():
+                    self.feats0[k] = torch.unsqueeze(
+                        self.feats0[k], 0
+                    )  # undo the squeezing we did before
+            feats1 = extractor.extract(image1_preprocessed.to(device))
+
+            # Get matches
+            matches01 = matcher({"image0": self.feats0, "image1": feats1})
+            self.feats0, feats1, matches01 = [
+                rbd(x) for x in [self.feats0, feats1, matches01]
+            ]  # remove batch dimension
+
+            # Return to original scale
+            if preprocessor.conf.scale != None:
+                feats1["keypoints"] = feats1["keypoints"] / preprocessor.conf.scale
+
+        feats0_keypoints, feats1_keypoints, matches = (
+            self.feats0["keypoints"],
+            feats1["keypoints"],
+            matches01["matches"],
+        )
+        feats0_matches, feats1_matches = (
+            feats0_keypoints[matches[..., 0]],
+            feats1_keypoints[matches[..., 1]],
+        )
+
+        return feats0_matches, feats1_matches
---
+++ pyfishsensedev/library/homography/utils.py
@@ -0,0 +1,102 @@
+from pathlib import Path
+from types import SimpleNamespace
+
+import cv2
+import kornia
+import numpy as np
+import torch
+
+
+def rbd(data: dict) -> dict:
+    """Remove batch dimension from elements in data"""
+    return {
+        k: v[0] if isinstance(v, (torch.Tensor, np.ndarray, list)) else v
+        for k, v in data.items()
+    }
+
+
+def read_image(path: Path, grayscale: bool = False) -> np.ndarray:
+    """Read an image from path as RGB or grayscale"""
+    if not Path(path).exists():
+        raise FileNotFoundError(f"No image at path {path}.")
+    mode = cv2.IMREAD_GRAYSCALE if grayscale else cv2.IMREAD_COLOR
+    image = cv2.imread(str(path), mode)
+    if image is None:
+        raise IOError(f"Could not read image at {path}.")
+    if not grayscale:
+        image = image[..., ::-1]
+    return image
+
+
+def numpy_image_to_torch(image: np.ndarray) -> torch.Tensor:
+    """Normalize the image tensor and reorder the dimensions."""
+    if image.ndim == 3:
+        image = image.transpose((2, 0, 1))  # HxWxC to CxHxW
+    elif image.ndim == 2:
+        image = image[None]  # add channel axis
+    else:
+        raise ValueError(f"Not an image: {image.shape}")
+    return torch.tensor(image / 255.0, dtype=torch.float)
+
+
+def load_image(path: Path, resize: int = None, **kwargs) -> torch.Tensor:
+    return numpy_image_to_torch(read_image(path))
+
+
+class Preprocessor:
+    def __init__(self, **conf):
+        self.default_conf = {
+            "contrast": None,
+            "gamma": 2.0,
+            "brightness": None,
+            "sharpness": 0.5,
+            "scale": 1.6,
+        }
+        self.conf = SimpleNamespace(**{**self.default_conf, **conf})
+
+    def __call__(self, img):
+        if self.conf.contrast != None:
+            img = kornia.enhance.adjust_contrast(img, self.conf.contrast)
+        if self.conf.gamma != None:
+            img = kornia.enhance.adjust_gamma(img, self.conf.gamma)
+        if self.conf.brightness != None:
+            img = kornia.enhance.adjust_brightness(img, self.conf.brightness)
+        if self.conf.sharpness != None:
+            img = kornia.enhance.sharpness(img, self.conf.sharpness)
+        if self.conf.scale != None:
+            img = img.unsqueeze(0)
+            original_h, original_w = img.shape[:2]
+            center = [original_w // 2, original_h // 2]
+            img = kornia.geometry.transform.scale(
+                img, torch.Tensor([self.conf.scale]), center=torch.Tensor([center])
+            )
+            img = img.squeeze(0)
+        return img
+
+
+class Extractor(torch.nn.Module):
+    def __init__(self, **conf):
+        super().__init__()
+        self.conf = SimpleNamespace(**{**self.default_conf, **conf})
+
+    @torch.no_grad()
+    def extract(self, img: torch.Tensor, **conf) -> dict:
+        """Perform extraction with online resizing"""
+        if img.dim() == 3:
+            img = img[None]  # add batch dim
+        assert img.dim() == 4 and img.shape[0] == 1
+        shape = img.shape[-2:][::-1]  # take last two and reverse order
+        # resize to use with superpoint
+        h, w = img.shape[-2:]
+        img = kornia.geometry.transform.resize(
+            img,
+            1024,
+            side="long",
+            antialias=True,
+            align_corners=None,
+        )
+        scales = torch.Tensor([img.shape[-1] / w, img.shape[-2] / h]).to(img)
+        feats = self.forward({"image": img})
+        feats["image_size"] = torch.tensor(shape)[None].to(img).float()
+        feats["keypoints"] = (feats["keypoints"] + 0.5) / scales[None] - 0.5
+        return feats
