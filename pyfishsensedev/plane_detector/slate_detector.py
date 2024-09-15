98d28008da7053ea2ef3563d9ceb4c3eabf3a318
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-09-14 22:35:08 -0700

    feat: for now, try many scales and choose the best.

--- pyfishsensedev/plane_detector/slate_detector.py
+++ pyfishsensedev/plane_detector/slate_detector.py
@@ -11,20 +11,37 @@ class SlateDetector(PlaneDetector):
         super().__init__(image)
 
         self._pdf = pdf
-        self._image_matcher = ImageMatcher(numpy_image_to_torch(self._pdf.image))
-        #     processing_conf={
-        #         "preprocess": {"gamma": 2.0, "sharpness": None, "scale": 1.6},
-        #         "extractor": {"max_num_keypoints": None},
-        #         "matcher": {"filter_threshold": 0.5},
-        #     },
-        # )
+        self._tensor_template = numpy_image_to_torch(self._pdf.image)
 
     def _get_template_matches(self):
-        feats0_matches, feats1_matches = self._image_matcher(
-            numpy_image_to_torch(self.image)
-        )
+        max_num_keypoints = 0
+        max_feats0_matches = None
+        max_feats1_matches = None
 
-        print(f"{feats0_matches.size()}, {feats1_matches.size()}")
+        for scale in range(1, 20):
+            scale = float(scale) / 10.0
+
+            image_matcher = ImageMatcher(
+                self._tensor_template,
+                processing_conf={
+                    "preprocess": {"gamma": 2.0, "sharpness": None, "scale": scale},
+                    "extractor": {"max_num_keypoints": None},
+                    "matcher": {"filter_threshold": 0.5},
+                },
+            )
+
+            feats0_matches, feats1_matches = image_matcher(
+                numpy_image_to_torch(self.image)
+            )
+
+            num_keypoints, _ = feats0_matches.shape
+            if num_keypoints > max_num_keypoints:
+                max_num_keypoints = num_keypoints
+
+                max_feats0_matches = feats0_matches
+                max_feats1_matches = feats1_matches
+
+        print(f"{max_feats0_matches.size()}, {max_feats1_matches.size()}")
 
     def _get_points_image_space(self):
         pass
