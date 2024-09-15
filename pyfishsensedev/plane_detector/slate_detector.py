0b82100d5504ed0fb51fcd58ea10cd8241975d7b
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-09-14 18:34:02 -0700

    run feature matcher

--- pyfishsensedev/plane_detector/slate_detector.py
+++ pyfishsensedev/plane_detector/slate_detector.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 from pyfishsensedev.image.pdf import Pdf
+from pyfishsensedev.library.homography.image_matcher import ImageMatcher
+from pyfishsensedev.library.homography.utils import numpy_image_to_torch
 from pyfishsensedev.plane_detector.plane_detector import PlaneDetector
 
 
@@ -9,6 +11,19 @@ class SlateDetector(PlaneDetector):
         super().__init__(image)
 
         self._pdf = pdf
+        self._image_matcher = ImageMatcher(
+            numpy_image_to_torch(self._pdf.image),
+            processing_conf={
+                "preprocess": {"gamma": 2.0, "sharpness": None, "scale": 1.6},
+                "extractor": {"max_num_keypoints": None},
+                "matcher": {"filter_threshold": 0.5},
+            },
+        )
+
+    def _get_template_matches(self):
+        feats0_matches, feats1_matches = self._image_matcher(numpy_image_to_torch(self.image))
+
+        print("Done")
 
     def _get_points_image_space(self):
         pass
