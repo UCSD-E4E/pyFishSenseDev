ccab6a93161a1468931a175aa1527e1fe0a21b41
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-11-13 15:05:02 -0800

    fix: properly export raw processor

--- pyfishsensedev/image/__init__.py
+++ pyfishsensedev/image/__init__.py
@@ -1,2 +1,2 @@
-from pyfishsensedev.image.image_processors.raw_processor_old import RawProcessor
+from pyfishsensedev.image.image_processors.raw_processor import RawProcessor
 from pyfishsensedev.image.image_rectifier import ImageRectifier
