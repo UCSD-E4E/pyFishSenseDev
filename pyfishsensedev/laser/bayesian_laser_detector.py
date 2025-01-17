cf1cbe9cbb043d12a1577c489b6137e76abd16df
benmcc <bbmcca26@g.holycross.edu>
2025-01-17 06:48:31 -0500

    feat: more efficient inferencing

--- pyfishsensedev/laser/bayesian_laser_detector.py
+++ pyfishsensedev/laser/bayesian_laser_detector.py
@@ -3,102 +3,141 @@ import cv2
 import sys
 import math
 import matplotlib.pyplot as plt
-from sklearn.mixture import GaussianMixture
+from sklearn.mixture import GaussianMixture, BayesianGaussianMixture
+import glob
 
-class PDFOneDim():
-    def __init__(self, meu, sigma):
-        self.meu = meu
-        self.sigma = sigma
-    def __call__(self, x):
-        coeff = 1.0 / (math.sqrt(2.0 * math.pi) * self.sigma)
-        exponent = -((x - self.meu) ** 2) / (2.0 * (self.sigma ** 2))
-        return coeff * math.exp(exponent)
+def pdf_multidim(X, mean, cov_inv, cov_det):
+    X = X.astype(np.float32)
+    d = X.shape[1]
+    diff = X - mean
+    exponent = -0.5 * np.einsum('ij,ij->i', diff @ cov_inv, diff)
+    denom = np.sqrt((2.0 * np.pi)**d * cov_det)
+    return (1.0 / denom) * np.exp(exponent)
+
+class GMM():
+    def __init__(self, k, samples):
+        self.gmm = BayesianGaussianMixture(n_components=k, max_iter=200, covariance_type='full')
+        self.gmm.fit(samples)
+
+        self.means = self.gmm.means_
+        self.covs = self.gmm.covariances_
+        self.weights = self.gmm.weights_
+        
+        self.k = k
+        self.cov_invs = []
+        self.cov_dets = []
+        for i in range(self.k):
+            cov_inv = np.linalg.inv(self.covs[i])
+            cov_det = np.linalg.det(self.covs[i])
+            self.cov_invs.append(cov_inv)
+            self.cov_dets.append(cov_det)
+
+    def pdf(self, X):
+        N = X.shape[0]
+        p = np.zeros(N, dtype=np.float64)
+        for i in range(self.k):
+            p_comp = pdf_multidim(X, self.means[i], self.cov_invs[i], self.cov_dets[i])
+            p += self.weights[i] * p_comp
+        return p
 
 class PDFMultiDim():
-    def __init__(self, meu, cov):
-        self.meu = np.asarray(meu)
-        self.cov = np.asarray(cov)
-        self.cov_det = np.linalg.det(self.cov)
+    def __init__(self, mean, cov):
+        self.mean = np.asarray(mean, dtype=np.float64)
+        self.cov = np.asarray(cov, dtype=np.float64)
         self.cov_inv = np.linalg.inv(self.cov)
-        self.denom = np.sqrt((2 * np.pi)**3 * self.cov_det)
-    def __call__(self, x):
-        x = np.asarray(x)
-        diff = x - self.meu
-        exponent = -0.5 * diff.T @ self.cov_inv @ diff
-        return (1.0 / self.denom) * np.exp(exponent)
+        self.cov_det = np.linalg.det(self.cov)
+
+    def pdf(self, X):
+        return pdf_multidim(X, self.mean, self.cov_inv, self.cov_det)
 
 def find_mean_cov(img):
     img32 = img.astype(np.float32)
     pixels = img32.reshape(-1, 3)
-    mean_hsv = np.mean(pixels, axis=0) 
+    mean_hsv = np.mean(pixels, axis=0)
     cov_hsv = np.cov(pixels, rowvar=False)
     return mean_hsv, cov_hsv
 
+def get_samples(path):
+    dirs = glob.glob(f"{path}*")
+    pixels = np.empty((0, 3), dtype=np.float32)
+
+    for p in dirs:
+        img = cv2.imread(p)
+        if img is None:
+            print(f"Error reading image: {p}")
+            continue
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # Convert to float32, then flatten
+        pixels = np.vstack((pixels, img.reshape(-1, 3).astype(np.float32)))
+    return pixels
+
 def main():
     path = sys.argv[1]
-    img_bgr = cv2.imread(path) # cv2's default color space is BGR
+    img_bgr = cv2.imread(path)
+    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+    if img_bgr is None:
+        print(f"Could not read image: {path}")
+        return
+
+    # Convert entire image to HSV
     img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
 
-    laser_pixels = []
+    # Flatten (H*W, 3)
+    H, W, _ = img_hsv.shape
+    pixels_hsv = img_hsv.reshape(-1, 3).astype(np.float32)
 
-    # Priors
-    prior_laser = 0.001 # 0.001
-    prior_bg = 0.999 # 0.999
+    # Priors - right now hard coded, should be related to how many total pixels in image
+    prior_laser = 0.00001 # 0.001
+    prior_bg = 0.99999 # 0.999
+
+    # Build background model
+    bg_mean, bg_cov = find_mean_cov(img_hsv)
+    pdf_bg = PDFMultiDim(bg_mean, bg_cov)
+
+    # Build laser model from GMM
+    laser_samples = get_samples("laser_data/")
+    gmm_laser = GMM(k=10, samples=laser_samples)
 
     # Likelihoods
-    laser_mean = [138.60448, 73.84311, 250.71721]
-    laser_cov = [[4127.75375298, 1053.89700979, -111.98989248],
-                [1053.89700979, 523.51362831, -53.9500694],
-                [-111.98989248, -53.9500694, 31.80453888]]
-    bg_mean, bg_cov = find_mean_cov(img_hsv)
-    p_laser = PDFMultiDim(laser_mean, laser_cov)
-    #p_bg = PDFOneDim(100, 20)
-    p_bg = PDFMultiDim(bg_mean, bg_cov)
-    # for each pixel
-    for r in range(0, len(img_hsv)):
-        for c in range(0, len(img_hsv[r])):
-            likelihood_laser = p_laser(img_hsv[r][c].astype(int))
-            #likelihood_bg = p_bg(int(img_bgr[r][c][2])) # only the red channel
-            likelihood_bg = p_bg(img_hsv[r][c].astype(int))
-            # Posterior
-            norm_const = likelihood_laser * prior_laser + likelihood_bg * prior_bg
-            posterior_laser = (likelihood_laser * prior_laser) / norm_const
-            posterior_bg = (likelihood_bg * prior_bg) / norm_const
-            #print(f"Laser posterior: {posterior_laser} | BG posterior: {posterior_bg}")
-            if posterior_laser > posterior_bg:
-                laser_pixels.append([r, c])
-    
-    print(laser_pixels)
+    likelihood_bg = pdf_bg.pdf(pixels_hsv)
+    likelihood_laser = gmm_laser.pdf(pixels_hsv)
 
-    for pair in laser_pixels:
-        r = pair[0]
-        c = pair[1]
-        img_bgr[r][c] = [128,0,0]
+    # Combine with priors => posterior
+    numerator_laser = likelihood_laser * prior_laser
+    numerator_bg = likelihood_bg * prior_bg
+    norm_const = numerator_laser + numerator_bg
 
-    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+    # Posterior for laser
+    posterior_laser = numerator_laser / norm_const
+    # Classify as laser if posterior_laser > posterior_bg => posterior_laser > 0.5
+    laser_mask = (posterior_laser > 0.5).reshape(H, W)
+
+    # Color the laser pixels in BGR
+    img_bgr[laser_mask] = [128, 0, 0]
+
+    # Plot centroid, indicating laser pixel
+    laser_rows, laser_cols = np.where(laser_mask)
+    if len(laser_rows) > 0:
+        centroid_row = np.mean(laser_rows)
+        centroid_col = np.mean(laser_cols)
+        print("Laser centroid at (row, col) =", centroid_row, centroid_col)
+
+        # Plot the image
+        img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+        plt.imshow(img_rgb)
+        # Plot the centroid
+        plt.scatter(
+            [centroid_col], [centroid_row],
+            color='red', marker='x', s=50, label='Laser Pixel Mean'
+        )
+    else:
+        print("No laser pixels found; cannot compute centroid.")
+
+    # Show result
+    plt.legend()
     plt.imshow(img_rgb)
+    plt.title("Laser Pixels (Blue)")
     plt.show()
-
-    #############################################
-
-    # test pdf
-    # avg = 225
-    # std_dev = 10
-    # pdf = PDFOneDim(avg, std_dev)
-
-    # x = [val for val in range(0,255)]
-    # y = [pdf(val) for val in x]
-
-    # data_x = reds.ravel()
-    # data_y = [pdf(int(val)) for val in data_x]
-
-    # plt.plot(x, y)
-    # plt.scatter(data_x, data_y, color="hotpink", label="Real data")
-    # plt.xlabel("Red channel")
-    # plt.ylabel("Probability density")
-    # plt.legend()
-    # plt.title("1-D Gaussian PDF (laser class)")
-    # plt.show()
 
 if __name__ == "__main__":
     main()
