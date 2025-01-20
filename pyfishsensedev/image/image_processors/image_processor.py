3242bb816b7b10649ed594a3eff8f58ad1bebf3a
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-01-20 13:14:27 -0800

    feat: fix structure of raw processor

--- pyfishsensedev/image/image_processors/image_processor.py
+++ pyfishsensedev/image/image_processors/image_processor.py
@@ -6,14 +6,6 @@ import numpy as np
 
 
 class ImageProcessor(ABC):
-    def __init__(self, file: Path) -> None:
-        super().__init__()
-
-        self.file = file
-
-    def __iter__(self) -> Self:
-        return self
-
     @abstractmethod
-    def __next__(self) -> np.array:
+    def process(self, file: Path) -> np.ndarray:
         raise NotImplementedError
--- pyfishsensedev/image/image_processors/raw_processor.py
+++ pyfishsensedev/image/image_processors/raw_processor.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import Self
 
 import cv2
 import numpy as np
@@ -10,24 +9,16 @@ from pyfishsensedev.image.image_processors.image_processor import ImageProcessor
 
 
 class RawProcessor(ImageProcessor):
-    def __init__(self, file: Path, gamma=0.3) -> None:
-        super().__init__(file)
+    def __init__(self, gamma=0.3) -> None:
+        super().__init__()
 
         self.__gamma = gamma
-        self.__has_iterated = True
 
-    def __iter__(self) -> Self:
-        self.__has_iterated = False
+    def __double_2_uint16(self, img: np.ndarray) -> np.ndarray:
+        return (img * 65535).astype(np.uint16)
 
-        return self
-
-    def __next__(self) -> np.ndarray:
-        if self.__has_iterated:
-            raise StopIteration
-
-        self.__has_iterated = True
-
-        with rawpy.imread(self.file.as_posix()) as raw:
+    def process(self, file: Path) -> np.ndarray:
+        with rawpy.imread(file.as_posix()) as raw:
             img = raw.postprocess(
                 gamma=(1, 1), no_auto_bright=True, use_camera_wb=True, output_bps=16
             )
@@ -35,6 +26,3 @@ class RawProcessor(ImageProcessor):
             img = skimage.exposure.equalize_adapthist(img)
 
             return cv2.cvtColor(self.__double_2_uint16(img), cv2.COLOR_RGB2BGR)
-
-    def __double_2_uint16(self, img: np.ndarray) -> np.ndarray:
-        return (img * 65535).astype(np.uint16)
